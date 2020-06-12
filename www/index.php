5f9c098a8d322853c31565f89ac687fbd4a6e1b1
mpageau <mpageau@criticalmix.com>
2020-06-11 22:17:50 -0400

    adding inital docker setup

---
+++ www/index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+  <title>big PP</title>
+</head>
+<body>
+  <?php
+    $arr = [1,2,3,4,5];
+    foreach ($arr as $key => $value) {
+      echo "<h1>pp $key is $value big</h1><br>";
+    }
+  ?>
+</body>
+</html>
